15f1ee9568f17a6c8129e903e7f022d251eab32e
Olivia Li <oliviali.ouyang@gmail.com>
2021-01-17 02:43:26 -0800

    adding in styled components

---
+++ webapp/src/App2.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import fire from './config';
+import styled from 'styled-components'
+
+export const App = () => {
+  return(
+    <>
+      <div className="imagediv1"></div>
+      <div className="imagediv2"></div>
+      <img src="https://i.redd.it/ldfi6xzs56t51.png" width="1235px" height="691px" />
+    </>
+
+  )
+
+}
+
+export default App;
