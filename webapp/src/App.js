a3e895248ccc96b1dc638aa149e8d50afe736d81
Nicholas Wang <nicholaswang2000@berkeley.edu>
2021-01-17 02:05:06 -0800

    Realtime updates

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
 import React, { Component } from 'react';
 import fire from './config';
+import { all } from 'q';
 
 class App extends Component {
-  state = { "blue": {x:20, y:20}, "red": {x:0, y:0}, "yellow": {x:50, y:25}, "lime": {x:30, y:75} }
+  state = { "blue": {x:10, y:40}, "red": {x:0, y:30}, "yellow": {x:50, y:25}, "lime": {x:30, y:75} }
 
   componentDidMount = () => {
-    this.updatePositions({"blue": {x: 200, y:0}})
-    const dotsRef = fire.database().ref('None');
+    const dotsRef = fire.database().ref('HMKBUF').child('webapp');
+
+    const colorDict = {maroon: 'red', firebrick: 'red', brown: 'red', rosybrown: 'red', darkolivegreen: 'red', blue: 'blue'}
+
     dotsRef.on('value', (snapshot) => {
       snapshot.forEach(data => {
-        console.log(data.val())
+        let theData = data.val()
+        console.log(colorDict[theData.color] + " is at " + theData.x + "," + theData.y)
+        var obj = {}
+        obj[colorDict[theData.color]] = colorDict[theData.color]
+        this.setState({[colorDict[theData.color]]: {x: Math.round(theData.x/3), y: Math.round(theData.y/3)}})
       })
     })
   }
 
   dotPositions = (dots) => {
-    var dict = {blue: "https://www.mikeball.com/wp-content/uploads/2016/03/blue-dot.png", red: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Basic_red_dot.png", yellow: "https://img.pngio.com/filegaudit-yellowdotpng-wikimedia-commons-yellow-dot-png-460_460.png", lime: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Location_dot_lime.svg/1024px-Location_dot_lime.svg.png"};
+    const dict = {blue: "https://www.mikeball.com/wp-content/uploads/2016/03/blue-dot.png", red: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Basic_red_dot.png", yellow: "https://img.pngio.com/filegaudit-yellowdotpng-wikimedia-commons-yellow-dot-png-460_460.png", lime: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Location_dot_lime.svg/1024px-Location_dot_lime.svg.png"};
     
     var dotMap = []
     for (const [key, value] of Object.entries(dots)) {
@@ -41,7 +48,7 @@ class App extends Component {
     const dotPos = this.dotPositions(this.state)
     return (
       <div>
-        <img src="https://i.redd.it/ldfi6xzs56t51.png" width="80%" height="auto" />
+        <img src="https://i.redd.it/ldfi6xzs56t51.png" width="1235px" height="691px" />
         {dotPos}
       </div>
     )
